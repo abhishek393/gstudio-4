86e72bea5f1ca3461623f77f05eaa677c02609d4
vickysingh17 <39724007+vickysingh17@users.noreply.github.com>
2018-06-09 13:39:33 +0530

    Add urls (#1)

---
+++ gnowsys_ndf/gnowsys_ndf/ndf/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+app_name = 'ndf'
+urlpatterns=[
+	url(r'^create/', views.create , name = 'create'),
+	url(r'^read/', views.read , name = 'read'),	
+   	url(r'^update/', views.update , name ='update'),
+	url(r'^delete/', views.delete , name = 'delete')]
