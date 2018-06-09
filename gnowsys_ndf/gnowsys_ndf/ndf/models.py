fa718eb910042e46890ae8d899cdd6c01affffee
Apurv Bajaj <apurvabajaj007@gmail.com>
2018-06-09 13:40:17 +0530

    Add Node model (#2)

--- gnowsys_ndf/gnowsys_ndf/ndf/models.py
+++ gnowsys_ndf/gnowsys_ndf/ndf/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
+import json
 from django.db import models
 
-# Create your models here.
+class Node(models.Model):
+    """The basic model for all objects."""
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    created_by = models.CharField(max_length=30)
+    last_update = models.DateTimeField()
+
+    def get_json(self):
+        """Converts the object into json."""
+        json_rep = {}
+        json_rep['name'] = self.name
+        json_rep['created_at'] = self.created_at
+        json_rep['created_by'] = self.created_by
+        json_rep['last_update'] = self.last_update
+        return json.dumps(json_rep)
