5323bf9221cf7ac2ecd46ae5abd53d2059639197
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-06-11 14:46:19 +0530

    Fix views

--- gnowsys_ndf/gnowsys_ndf/ndf/views.py
+++ gnowsys_ndf/gnowsys_ndf/ndf/views.py
@@ -1,39 +1,46 @@
+import os
+import json
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
-import os
 from .models import Node
 from django.utils import timezone
 
 
-@static_vars(ver=0)
 def create(request):
-	if request.method == 'POST':
-		data = request.POST
-		node_obj = Node(name = data.get('name'), created_at = data.get('created_at'),created_by = data.get('created_by'),last_update = data.get('last_update'))
-		currunt_dir = os.pwd()
-		path_to_data_dir = os.path.join(currunt_dir, '/data/')
-		fl = open(path_to_data_dir + "node_file_" + ver + ".json", "w+")
-		json_file = node_obj.get_json()
-		fl.write(json_file)
-		ver = ver + 1
-		return HttpResponse((json.dumps('SUCCESS' : 'SUCCESS')), content_type="application/json")
-	else:
-		return HttpResponse((json.dumps('ERROR' : 'Wrong request method')), content_type="application/json")
-		
-	
-def read(request):
-	data = request.POST
-	ver_id=data.get('version_id')
-		
-	return HttpResponse((), content_type="application/json")
+    if request.method == 'POST':
+        data = request.POST
+        node_obj = Node(
+            name=data.get('name'),
+            created_at=data.get('created_at'),
+            created_by=data.get('created_by'),
+            last_update=data.get('last_update'))
+        currunt_dir = os.getcwd()
+        path_to_data_dir = os.path.join(currunt_dir, '/data/')
 
-	
+        try:
+            create.version_id += 1
+        except AttributeError:
+            create.version_id = 0
+
+        json_file = open(path_to_data_dir + 'node_file_' + create.version_id + '.json', 'w+')
+        json_data = node_obj.get_json()
+        json_file.write(json_data)
+        return HttpResponse(json.dumps({'SUCCESS': 'SUCCESS'}), content_type='application/json')
+    else:
+        return HttpResponse(
+            json.dumps({'ERROR': 'Wrong request method'}), content_type='application/json')
+
+
+def read(request):
+    data = request.POST
+    version_id = data.get('version_id')
+
+    return HttpResponse(json.dumps({'TBD': 'TBD'}), content_type='application/json')
+
+
 def update(request):
-	pass
+    pass
 
 
 def delete(request):
-	pass
-
+    pass
