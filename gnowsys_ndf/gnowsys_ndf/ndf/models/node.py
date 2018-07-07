1e69d42d0fb3440f0e4e09704d4b487b8f1f5e3f
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-07-07 21:35:14 +0530

    Lint fixes

--- gnowsys_ndf/gnowsys_ndf/ndf/models/node.py
+++ gnowsys_ndf/gnowsys_ndf/ndf/models/node.py
@@ -1,8 +1,8 @@
 import json
+import uuid
 from django.db import models
 from django.http import HttpResponse
 from elasticsearch import Elasticsearch
-import uuid
 
 es = Elasticsearch()
 
@@ -24,74 +24,82 @@ class Node(models.Model):
         return json.dumps(json_rep)
 
     def __get_id(self):
-        self.object_id = uuid.uuid4()    
+        self.object_id = uuid.uuid4()
 
     def create(self):
-        """This function converts the Node object into a JSON file and 
-            indexes the JSON objects in the ElasticSearch server at the 
-            same instance so that it can be used by other operations. 
-            It returns a success or failure message depending on whether the operation is successful."""
+        """This function converts the Node object into a JSON file and
+            indexes the JSON objects in the ElasticSearch server at the
+            same instance so that it can be used by other operations.
+            It returns a success or failure message depending on whether
+            the operation is successful."""
 
         json_data = self.get_json()
         self.__get_id()
-        result = es.index(index="data", doc_type='node', id = self.object_id, body=json_data)
+        result = es.index(index="data", doc_type='node', id=self.object_id, body=json_data)
         return result
 
     @staticmethod
     def read(data):
-        """This function receives a search query from the Client. 
-            It then fetches the search query and uses ElasticSearch to find 
-            the corresponding JSON object. The Read function then returns the JSON object found."""
+        """This function receives a search query from the Client.
+            It then fetches the search query and uses ElasticSearch to find
+            the corresponding JSON object. The Read function then returns
+            the JSON object found."""
         data_dict = dict(data)
         json_query = {}
         for key in data_dict.keys():
             json_query[key] = data_dict[key][0]
-        searched_data = es.search(index='data', doc_type='node', body={'query': {'match': json_query}})
+        searched_data = es.search(
+            index='data', doc_type='node', body={'query': {'match': json_query}})
         return HttpResponse(json.dumps(searched_data), content_type='application/json')
 
     @staticmethod
     def update(data):
         """This function receives the field to be modified or updated,
-            as { Key: Value } pairs from the Client.It then uses ElasticSearch 
-            to find the corresponding object and then updates the respective field. 
-            It returns a success or failure message depending on whether the operation is successful."""
+            as { Key: Value } pairs from the Client.It then uses ElasticSearch
+            to find the corresponding object and then updates the respective field.
+            It returns a success or failure message depending on whether the
+            operation is successful."""
         data_dict = dict(data)
         json_query = {}
         for key in data_dict.keys():
-            json_query[key] = data_dict[key][0]        
+            json_query[key] = data_dict[key][0]
         json_query_keys = list(json_query)
         value = json_query[json_query_keys[0]]
         searched_list = value.split(',')
         reqd_dict = {}
         reqd_dict[json_query_keys[0]] = searched_list[0]
-        searched_data = es.search(index='data', doc_type='node', body={'query': {'match': reqd_dict}})
+        searched_data = es.search(
+            index='data', doc_type='node', body={'query': {'match': reqd_dict}})
         searched_data_ids = []
         for hit in searched_data['hits']['hits']:
             searched_data_ids.append(hit['_id'])
-        for counter,data_id in enumerate(searched_data_ids):
+        for counter, data_id in enumerate(searched_data_ids):
             searched_data['hits']['hits'][counter]['_source'][json_query_keys[0]] = searched_list[1]
             res = es.index(
                 index="data",
                 doc_type='node',
                 id=data_id,
                 body=searched_data['hits']['hits'][counter]['_source'])
-        if res:        
+        if res:
             return HttpResponse(json.dumps({'SUCCESS':'SUCCESS'}), content_type='application/json')
 
     @staticmethod
     def delete(data):
-        """This function receives a criteria for which the JSON object has to be deleted
-            , as { Key: Value } pairs. It uses ElasticSearch server to find all the files 
-            which match the criteria and then deletes it from the ElasticSearch index. 
-            It returns a success or failure message depending on whether the operation is successful"""
+        """This function receives a criteria for which the JSON object has to be deleted,
+            as { Key: Value } pairs. It uses ElasticSearch server to find all the files
+            which match the criteria and then deletes it from the ElasticSearch index.
+            It returns a success or failure message depending on whether
+            the operation is successful"""
         data_dict = dict(data)
         json_query = {}
         for key in data_dict.keys():
             json_query[key] = data_dict[key][0]
-        searched_data = es.search(index='data', doc_type='node', body={'query': {'match': json_query}})
+        searched_data = es.search(
+            index='data', doc_type='node', body={'query': {'match': json_query}})
         searched_data_ids = []
         for hit in searched_data['hits']['hits']:
             searched_data_ids.append(hit['_id'])
         for data_id in searched_data_ids:
-            res = es.delete(index="data", doc_type='node', id= data_id)
-        return HttpResponse(json.dumps({'SUCCESS': 'SUCCESS'}), content_type='application/json')
+            res = es.delete(index="data", doc_type='node', id=data_id)
+        if res:
+            return HttpResponse(json.dumps({'SUCCESS': 'SUCCESS'}), content_type='application/json')
